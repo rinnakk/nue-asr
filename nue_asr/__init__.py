1741026edc2a462a684a5b3705071346bcfd7516
Yukiya Hono <27275016+yky-h@users.noreply.github.com>
2023-12-08 19:22:29 +0900

    Maintenance (#1)
    * change project name
    * use logging
    * add warning for long audio

--- nue_asr/__init__.py
+++ nue_asr/__init__.py
@@ -1,3 +1,7 @@
+import logging
+
 from .model import NueASRConfig, NueASRModel  # noqa: F401
 from .transcribe import transcribe  # noqa: F401
 from .utils import load_model, load_tokenizer  # noqa: F401
+
+logging.getLogger(__name__)
